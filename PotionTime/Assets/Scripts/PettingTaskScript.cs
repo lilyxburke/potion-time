3103ebc7e1c786de39baca62cf09fa56bd1eb26b
lilyxburke <lburke701@gmail.com>
2024-08-06 11:45:44 +0100

    clicking on tasks take you to seperate scenes, started petting task

---
+++ PotionTime/Assets/Scripts/PettingTaskScript.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+
+public class PettingTaskScript : MonoBehaviour
+{
+    private bool submitted = false;
+    private int[] correctClicks;
+    private int[] chosenClicks = new int[] {0, 0, 0, 0, 0, 0};
+
+    void Start()
+    {
+        correctClicks = new int[] {1, 0, 0, 0, 0, 0};
+    }
+
+    public void CheckAnswer()
+    {
+        if (correctClicks.SequenceEqual(chosenClicks))
+        {
+            submitted = true;
+        }
+    }
+
+    public void ResetClicks()
+    {
+        chosenClicks = new int[] { 0, 0, 0, 0, 0, 0 };
+    }
+
+    public void AddClicks(Button clickedButton)
+    {
+        string buttonName = clickedButton.name;
+
+        switch (buttonName)
+        {
+            case "LeftEar":
+                chosenClicks[0]++;
+                break;
+            case "RightEar":
+                chosenClicks[1]++; 
+                break;
+            case "Stomach":
+                chosenClicks[2]++;
+                break;
+            case "Back":
+                chosenClicks[3]++;
+                break;
+            case "LeftPaw":
+                chosenClicks[4]++;
+                break;
+            case "RightPaw":
+                chosenClicks[5]++;
+                break;
+        }
+    }
+
+    
+
+}
---
+++ PotionTime/Assets/Scripts/TaskClick.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TaskClick : MonoBehaviour
+{
+    public GameObject chosenTask;
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(chosenTask.name.Replace("(Clone)", "").Trim() + "Scene");
+    }
+
+}
